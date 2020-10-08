75115c20bcf7433dbbbd377ce772f9fbc21c105a
alageev <alageev01@gmail.com>
2020-10-08 13:50:32 +0300

    Initial Commit

---
+++ OSLab1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  OSLab1
+//
+//  Created by Алексей Агеев on 08.10.2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
